216be70a6d3709c653488067bf36259df56f7239
Kaic Bastidas <kaicbastidas@gmail.com>
2017-07-22 01:43:22 -0300

    droping base before inserting values (no repeated values)

--- run.rb
+++ run.rb
@@ -7,6 +7,7 @@ client = Mongo::Client.new('mongodb://127.0.0.1:27017/exileinfo')
 
 # Select collection, read from the API and insert on the base
 collection = client[:accounts]
+collection.drop
 
 url = 'http://api.pathofexile.com/ladders/Standard'
 uri = URI(url)
